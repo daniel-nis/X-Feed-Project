e1d114ba9ef70cb3c457c49ab9bb5924b13f3468
daniel-nis <72157039+daniel-nis@users.noreply.github.com>
2023-08-14 17:44:33 -0700

    added post viewer component and relative time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import Link from "next/link";
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
 import { SignInButton, useUser, SignOutButton } from "@clerk/nextjs";
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const {user} = useUser();
+
+  console.log(user);
 
   if (!user) return null;
 
@@ -13,10 +19,27 @@ const CreatePostWizard = () => {
     <img 
       src={user.profileImageUrl} 
       alt="Profile image"
-      className="h-16 w-16 rounded-full"
+      className="h-14 w-14 rounded-full"
     />
     <input placeholder="type some emojis" className="bg-transparent grow outline-none"/>
   </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="flex p-4 border-b border-slate-400 gap-3">
+      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full"/>
+      <div className="flex flex-col">
+        <div className="flex text-slate-300 gap-1">
+          <span>{`@${author.username}`}</span>
+          <span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
   );
 }
 
@@ -37,7 +60,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex justify-center h-screen">
-        <div className="w-full md:max-w-xl border-x border-slate-400">
+        <div className="h-full w-full md:max-w-2xl border-x border-slate-400">
           <div className="border-b border-slate-400 p-4 flex">
             {!user.isSignedIn && (
               <div className="flex justify-center">
@@ -48,8 +71,8 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col">
-            {[...data, ...data].map((post) => (
-              <div key={post.id} className="p-8 border-b border-slate-400">{post.content}</div>
+            {[...data, ...data]?.map((fullPost) => (
+                <PostView {...fullPost} key={fullPost.post.id} />
               ))}
           </div>
         </div>
