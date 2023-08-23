353bbac1d7642040ca794023094edc78a7f00c40
daniel-nis <72157039+daniel-nis@users.noreply.github.com>
2023-08-22 18:47:39 -0700

    created profile routing

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,19 +5,12 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-import type { User } from "@clerk/nextjs/api";
 import { TRPCError } from "@trpc/server";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
